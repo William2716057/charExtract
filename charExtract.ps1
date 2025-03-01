ebd6843ed2c30d55f6c8ef6baeaa5b9fad91444a
User <Willspri@gmail.com>
2025-03-02 12:32:08 +1300

    updated

--- charExtract.ps1
+++ charExtract.ps1
@@ -1,7 +1,7 @@
 #extract substring
 $string = Read-Host "Enter text"
 #extrancts from index[0] to index[1]
-$index0 = Read-Host
-$index1 = Read-Host
+$index0 = Read-Host "Enter beginning index"
+$index1 = Read-Host "Enter final index"
 $substring = $string.Substring($index0, $index1) 
 Write-Host $substring
